9cbc07abe90b2318d4f68d4f0be793a06a6e1cd6
a.medisetti <a.medisetti@finmont.com>
2023-09-26 11:44:25 +0530

    updated the src and the added the feature of customizing styles

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -18,59 +18,111 @@ const App = () => {
   const key =
     "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6ImFsbCB0b2tlbmlzYXRpb24gYXV0aG9yaXNhdGlvbiIsIm1lcmNoYW50UmVmZXJlbmNlIjoiZTY2MDRiY2YtYTFiNS00ZTJlLWIzZDAtYTFhNmE4OWIzZjA5IiwiZXhwIjoyMDUxMDU4MjM4LCJpc3MiOiJhcGktdG9rZW4tc2VydmljZSJ9.CxeveIRYjl_p8pJ5z7alqj20IhYBr_sq9IWOmzSY1EuTEDhT3rR5cfbmfaZtnL-vyhPUI46hZMNzz2bXNGmtrdrcXyank1OZIChTa-tsR4ZT08u3aN56KzzDSB9j-l08NdNgTWIctWp5pUfCm0to_MjlagrznoYghyu-RbP51Kf85kaH2SlKTFInwWIaT5L_5eDEGERItSFsh67L5Oev44VpAlxKSUpRqpgf9Ej6jTXzqUGhn_a0qwPwLBd7zTC7qsnKNt_VVDe2O3ddgs9FGJMkvWNiLwEqz47ffP4nsm-9q52cSH3gylfx8z8wEs0ZL9ZcGTUb0BEwnFpadO3hZbTMVEUjoHzQZloRXVJg3vTzl1z23wF0ra7N18KY69VT4cc_m76eYYU-QyqaGfi-dxUrJlZzdx1bVkDMnsZyK3MC4TGnhV9TGWbkPNaxxxJGIlrcUQnaZRm494zpIOHAMpoCKpYe1yxF-0qKH5_sr4qYkpKuEy_d1mn74Oy0p2fqaXLqnmbsg1ALcQt5zgL-WyKpo8n_n4EHf7YuN6ZrJl23l-0VEo9AG-7kcK1l2icjfPFL5-_rTa04E_DeuCowBOBWgXqqpvhE5W3OASc8cy9pN2pMbOZYfD_r2vVw8OMHCmFMceIZgkprP4j_hDe1Ml3LoAdtFUtux8jSrOte6T4"; // Replace with your API key
 
-  const customStyles = {
-    widget: {
-      backgroundColor: 'white',
-      width: '50%',
-      maxWidth: '80%',
-      padding: '20px',
-      border: '1px solid gray',
-    },
-    cardFormWidget: {
-      // Define custom styles for CardFormWidget here
-    },
+  const widgetStyles = {
+    customStyles: {
+      widget: {
+        width: '60%',
+        backgroundColor: 'white',
+        padding: '20px',
+        border: '1px solid gray',
+        borderRadius: '5px',
+      },
+      cardStyles: {
+        textStyles: {
+          head: {
+            color: "red",
+            fontSize: "15px",
+            fontWidth: "bold",
+          },
+          body: {
+            color: "black",
+            fontSize: "15px",
+            fontWidth: "normal",
+          },
+        },
+        cardNumberInput: {
+          width: '80%',
+          backgroundColor: 'white',
+          border: '1px solid gray',
+          padding: '5px',
+          marhin: '2px',
+          borderRadius: '5px',
+        },
+        expirationDateInput: {
+          width: '80%',
+          backgroundColor: 'white',
+          border: '1px solid gray',
+          padding: '5px',
+          marhin: '2px',
+          borderRadius: '5px',
+        },
+        cvvInput: {
+          width: '80%',
+          backgroundColor: 'white',
+          border: '1px solid gray',
+          padding: '5px',
+          marhin: '2px',
+          borderRadius: '5px',
+        },
+        feedback: {
+          color: 'red',
+          fontSize: '14px',
+        },
+        invalid: {
+          borderColor: 'red',
+        },
+        success: {
+          color: 'green',
+          fontWeight: 'bold',
+        },
+      },
+    }
   };
-  const order={
+
+  /* const order = {
     amount: {
-        currency: "GBP",
-        value: 100
+      currency: "GBP",
+      value: 100
     },
     customerEmail: "shopper@gmail.com",
     order: {
-        reference: "1255",
-        orderPayloadReference : "8bb1558a-21ac-43c6-8c20-de6c5572023e"
+      reference: "1255",
+      orderPayloadReference: "8bb1558a-21ac-43c6-8c20-de6c5572023e"
     },
     billingAddress: {
-        city: "Berlin",
-        country: "CA",
-        houseNumberName: "Laurens Cl",
-        name: "John Doe",
-        postalCode: "872823",
-        stateProvince: "Berl",
-        street: "Laurens"
+      city: "Berlin",
+      country: "CA",
+      houseNumberName: "Laurens Cl",
+      name: "John Doe",
+      postalCode: "872823",
+      stateProvince: "Berl",
+      street: "Laurens"
     },
     browserInfo: {
-        acceptHeader: "*/*",
-        colorDepth: 24,
-        ip: "192.168.12.1",
-        javaEnabled: true,
-        jsEnabled: true,
-        language: "EN",
-        name: "Firefox",
-        screenHeight: 580,
-        screenWidth: 443,
-        timezoneOffset: 0,
-        userAgent: "Chrome/51.0.2704.103 Safari/537.36",
-        version: "15"
+      acceptHeader: "",
+      colorDepth: 24,
+      ip: "192.168.12.1",
+      javaEnabled: true,
+      jsEnabled: true,
+      language: "EN",
+      name: "Firefox",
+      screenHeight: 580,
+      screenWidth: 443,
+      timezoneOffset: 0,
+      userAgent: "Chrome/51.0.2704.103 Safari/537.36",
+      version: "15"
     },
     card: {
-        token: "67ae538a-1559-4e63-b416-08b91c02368c",
-        type: "visa"
+      token: "67ae538a-1559-4e63-b416-08b91c02368c",
+      type: "visa"
     },
-    returnUrl:"http://localhost:3001",
-}
+    returnUrl: "http://localhost:3001",
+  } */
+
+  configureSdk(key, "dev");
 
   const [token, setToken] = useState(''); // State to store the token
+  const { Widget, get3DSResponse, getPaymentResponse } = useSdk();
 
   // Callback function to handle the received token
   const handleTokenReceived = (receivedToken: string) => {
@@ -78,9 +130,6 @@ const App = () => {
     // You can perform further actions with the token here
   };
 
-  configureSdk(key, "dev");
-
-  const { get3DSResponse } = useSdk(); // Assuming the SDK provides a function named get3DSResponse
 
   const callGet3DSResponse = async () => {
     try {
@@ -95,52 +144,33 @@ const App = () => {
 
   useEffect(() => {
     // Configure the SDK with your API key and environment (e.g., "dev")
-    if(recieptReference && ref){
+    if (recieptReference && ref) {
       callGet3DSResponse();
     }
   }, [key]);
-  const { Widget, authorizeCard } = useSdk();
-  const orderReq = {
-    ...order,
-    card:{
-      token: token,
-      type: 'visa'
-    }
-  };
-  const { getPaymentResponse } = useSdk()
-  const getPaymentStatus = async() => {
-    if(recieptReference) {
+
+
+  const getPaymentStatus = async () => {
+    if (recieptReference) {
       const resp = await getPaymentResponse(recieptReference, key, "dev");
       console.log(resp);
     }
   }
 
-  
-
-  // Define a function to handle the authorization
-  const handleAuthorizeCard = () => {
-    if(token){
-      console.log("calling");
-    const res= authorizeCard(orderReq);
-    console.log(res);
-    }
-  };
-
   return (
-    <>
+    <div style={{display:'flex', flexDirection:'column'}}>
       <Widget
         type="card"
-        customStyles={customStyles}
+        widgetStyles={widgetStyles}
         onTokenReceived={handleTokenReceived} // Pass the callback function to the CardFormWidget
       />
-      <button onClick={handleAuthorizeCard} type="button">
-        Pay Securely
-      </button>
       {/* Display the token */}
       {token && <div>Received Token: {token}</div>}
-      <button type="button" onClick={()=>getPaymentStatus()}  >getPayment status</button>
-      <button type="button" onClick={()=>callGet3DSResponse()}  >getPayment 3Ds</button>
-    </>
+      <div style={{display:'flex', margin:'12px'}}>
+      <button style={{padding:'10px', borderRadius: '5px',margin:'12px'}} type="button" onClick={() => getPaymentStatus()}  >getPayment status</button>
+      <button style={{padding:'10px', borderRadius: '5px', margin:'12px'}} type="button" onClick={() => callGet3DSResponse()}  >getPayment 3Ds</button>
+      </div>
+    </div>
   );
 };
 
--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,46 +1,103 @@
-// Widget.js
 import React from 'react';
 import CardFormWidget from './CardFormWidget';
 
 export interface WidgetProps {
-  customStyles?: {
-    widget?: React.CSSProperties; // Styles for the widget container
-    cardFormWidget?: {
-      cardNumberInput?: React.CSSProperties; // Styles for cardNumber input
-      expirationDateInput?: React.CSSProperties; // Styles for expirationDate input
-      cvvInput?: React.CSSProperties; // Styles for cvv input
+  widgetStyles?: {
+    customStyles: {
+      widget?: React.CSSProperties;
+      cardStyles?: {
+        textStyles:{
+          head?: React.CSSProperties;
+          body?: React.CSSProperties;
+        }
+        cardNumberInput?: React.CSSProperties;
+        expirationDateInput?: React.CSSProperties;
+        cvvInput?: React.CSSProperties;
+        feedback?: React.CSSProperties;
+        invalid?: React.CSSProperties;
+        success?: React.CSSProperties;
+      };
     };
   };
   type: string;
   onTokenReceived: (token: string) => void;
 }
 
-/* export interface MySDKConfig {
-  apiKey: string;
-  baseUrl: string;
-} */
+const defaultStyles = {
+  
+  widget: {
+    backgroundColor: 'lightgray',
+    padding: '20px',
+    border: '1px solid gray',
+    borderRadius: '5px',
 
-
-
-const widgetStyles = {
-  // Example styles for the widget container
-  backgroundColor: 'lightblue',
-  padding: '20px',
-  border: '1px solid gray',
+  },
+  cardStyles: {
+    textStyles:{
+      head:{
+        color:"red",
+        fontSize: "15px",
+        fontWidth: "bold",
+      },
+      body:{
+        color:"black",
+        fontSize: "15px",
+        fontWidth: "normal",
+      },
+    },
+    cardNumberInput: {
+      width: '80%',
+      backgroundColor: 'white',
+      border: '1px solid gray',
+      padding: '6px',
+      margin: '2px',
+      borderRadius: '5px',
+    },
+    expirationDateInput: {
+      width: '80%',
+      backgroundColor: 'white',
+      border: '1px solid gray',
+      padding: '6px',
+      margin: '2px',
+      borderRadius: '5px',
+    },
+    cvvInput: {
+      width: '80%',
+      backgroundColor: 'white',
+      border: '1px solid gray',
+      padding: '6px',
+      margin: '2px',
+      borderRadius: '5px',
+    },
+    feedback: {
+      color: 'red',
+      fontSize: '14px',
+    },
+    invalid: {
+      borderColor: 'red',
+    },
+    success: {
+      color: 'green',
+      fontWeight: 'bold',
+    },
+  },
 };
 
 // Widget component
-export const Widget: React.FC<WidgetProps> = ({ customStyles, type, onTokenReceived }) => {
- 
-  if (type.toLowerCase() === "card") {
-    const cardWidgetStyles = {
-      ...widgetStyles, // Apply the base styles for the widget container
-      ...customStyles?.widget, // Apply custom styles for the widget container if provided
-    };
+export const Widget: React.FC<WidgetProps> = ({ widgetStyles, type, onTokenReceived }) => {
+  const mergedWidgetStyles = {
+    ...defaultStyles.widget,
+    ...(widgetStyles?.customStyles?.widget || {}), // Merge widget styles if they exist
+  };
+  const mergedCardStyles = {
+    ...defaultStyles.cardStyles,
+    ...(widgetStyles?.customStyles?.cardStyles || {}), // Merge card styles if they exist
+  };
 
+  if (type.toLowerCase() === "card") {
     return (
-      <div style={cardWidgetStyles}>
-        <CardFormWidget customStyles={customStyles?.cardFormWidget}  onTokenReceived={onTokenReceived}/>
+      <div style={mergedWidgetStyles}>
+        <CardFormWidget customStyles={mergedCardStyles} onTokenReceived={onTokenReceived} />
       </div>
     );
   } else if (type.toLowerCase() === "wallet") {
@@ -48,7 +105,6 @@ export const Widget: React.FC<WidgetProps> = ({ customStyles, type, onTokenRecei
   } else {
     return <h1>BankMethodForm</h1>;
   }
-}
+};
 
 export default Widget;
-// sdk.ts
