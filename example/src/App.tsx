9833ff8830c58ed383615a959b579703e099b61e
a.medisetti <a.medisetti@finmont.com>
2023-10-31 17:09:26 +0530

    update in the mediaQueries of the modal

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
 
   const [res, setRes] = useState<any>();
 
-  const [url3ds,] = useState<any>("https://pci-api-demo.airwallex.com/pa/card3ds/hk/three-ds-method/redirect/start?key=5c9feba2-6afa-4a63-9050-77a3dd8e6b9e");
+  const [url3ds,] = useState<any>("https://pci-api-demo.airwallex.com/pa/card3ds/hk/three-ds-method/redirect/start?key=3e6a0963-ce81-4d75-bbea-105d8a725f4d");
   const key =
     "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6InNkayIsIm1lcmNoYW50UmVmZXJlbmNlIjoiZTY2MDRiY2YtYTFiNS00ZTJlLWIzZDAtYTFhNmE4OWIzZjA5IiwiZXhwIjoyMDU3NjA0NzY4LCJpc3MiOiJhcGktdG9rZW4tc2VydmljZSJ9.QrOvIY6GLUF1cmrDX0QJbrcANcAwDkQm8xQ2TYO3EL5zsHH5KjFEGgjdamHz3lbvSddw2G9ZXBiSBnN-IFC-5FFNprcdCx6YiHDESLuLBVnjudLCAOCH6yDC0gqYDQmjUGlb1VpNmVt-9U7e-FGF3N2mxHcrkHWWhlXgJeFT28Na3PXImTXtfi2mtD5iWzb9AW_pu-MeXzWa_TuVgui3RBTQZOYU2XieK8SP91mhM_YOGIvL74GQ1WdySWgx_3Ji-5uaN-qPWi-kFjxPoZfGIs90MRPZ4Q6zmVWt-tM-OlD1e4Oal7WBBhLJlNarxZBgiOQLJRRjdXNHfvK3tCwMPjnSv4T55N40UdZeJDnFEetHGFnYRYYeseTkVykYzPUEQLlYy08vtz7ljhJ9d3VFwOUBij0pEB4S3q2wsON8_Y1DpktSc0e5ic_fzlIFLKLXoIeQ--zNvANQbUvloaZHVaeQZC6wFs9hm8eVLIX0c5SMxe8ONaWKCWPv6IEq2eJOaIzbjNK9C_l72NMVC9Eu4GDhlfo9sHMoA3lNMrLP6K6nXRWFR8xzjVbn5DPOE-wJCSxrGa3ZKhlUGP5Skccyfn-Z-KEUzlB22q1UTJj3byItwLJIw-yfglCZ2a3ypaN7nKCxZF-ATwPw_JySgyBPIu5CQvYe5E7cKerNh6yNUz8";
   configureSdk(key, "dev");
--- src/components/Modal3DS.tsx
+++ src/components/Modal3DS.tsx
@@ -1,14 +1,16 @@
 import React, { useRef, useEffect, useCallback, useState } from "react";
-import Modal from "react-modal";
+import { Modal } from "react-responsive-modal";
 import { get3DSResponse } from "../services/response3Ds";
+import 'react-responsive-modal/styles.css';
 import { SpinnerCircular } from "spinners-react";
+import styles from './Modal3DS.css'
 
 const Modal3DS: React.FC<{ isOpen: boolean; onClose: () => void; url: string; setRes: any }> = ({ isOpen, onClose, url, setRes }) => {
   const iframeRef = useRef<HTMLIFrameElement | null>(null);
   const [iframeUrl, setIframeUrl] = useState(url);
 
 
-  const callGet3DSResponse = useCallback(async (receiptRef: string,ref: string) => {
+  const callGet3DSResponse = useCallback(async (receiptRef: string, ref: string) => {
     try {
       const result = await get3DSResponse(receiptRef, ref);
       setRes(result);
@@ -34,7 +36,7 @@ const Modal3DS: React.FC<{ isOpen: boolean; onClose: () => void; url: string; se
         const reference = params['reference'];
         const receiptReference = params['receiptReference'];
         if (receiptReference && reference) {
-          callGet3DSResponse(receiptReference,reference);
+          callGet3DSResponse(receiptReference, reference);
         }
       }
     }
@@ -61,32 +63,18 @@ const Modal3DS: React.FC<{ isOpen: boolean; onClose: () => void; url: string; se
 
   return (
     <Modal
-      isOpen={isOpen}
-      onRequestClose={onClose}
-      shouldCloseOnOverlayClick={false} // Prevent closing on overlay click
-      shouldCloseOnEsc={false} // Prevent closing on pressing Esc key
-      // @ts-ignore
-      style={{
-        overlay: {
-          backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        },
-        content: {
-          backgroundColor: 'white',
-          border: 'none',
-          padding: '0',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          width: '50%',
-          height: '60%',
-          left: '25%',
-          top: '20%',
-        },
-      }}
-      contentLabel="3ds Modal"
-      ariaHideApp={false}
-    >
-      {iframeUrl!== url ? ( // Display loading spinner while isLoading is true
+        open={isOpen}
+        onClose={onClose}
+        closeOnEsc={false}
+        closeOnOverlayClick={false}
+        center
+        classNames={{
+          overlay: styles.customOverlay,
+          modal: styles.customModal,
+        }}
+      >
+        <div style={{width:"100%", height:"100%", display:"flex", justifyContent:"center", alignItems:"center"}}>
+      {iframeUrl !== url ? ( // Display loading spinner while isLoading is true
         <SpinnerCircular
           size={40} // Adjust the size as needed
           thickness={70} // Adjust the thickness as needed
@@ -99,27 +87,15 @@ const Modal3DS: React.FC<{ isOpen: boolean; onClose: () => void; url: string; se
           src={iframeUrl}
           id="myIframe"
           style={{
-            pointerEvents: 'auto',
-            border: 'none',
-            width: '90%',
-            height: '100%',
-            outline: 'none',
+            pointerEvents: "auto",
+            border: "none",
+            width: "90%",
+            height: "100%",
+            outline: "none",
           }}
         />
       )}
-      <button
-        onClick={onClose}
-        style={{
-          backgroundColor: "transparent",
-          border: 'none',
-          fontSize: '20px',
-          marginTop: '10px',
-          alignSelf: iframeUrl === url ? 'flex-start' : 'flex-end',
-          display: iframeUrl === url ? 'flex' : 'none',
-        }}
-      >
-        &times;
-      </button>
+      </div>
     </Modal>
   );
 };
