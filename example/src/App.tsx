16324168183f93ca4c2d13f4b69f4ca3bd3a0dbe
a.medisetti <a.medisetti@finmont.com>
2023-12-06 11:38:36 +0530

    added the isAuth field to the Modal at 3ds authentication

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -9,7 +9,8 @@ const App = () => {
 
   const [res, setRes] = useState<any>();
 
-  const [url3ds,] = useState<any>("https://pci-api-demo.airwallex.com/pa/card3ds/hk/three-ds-method/redirect/start?key=e6fb1096-8eb7-431d-842b-444376cc5ae8");
+  const [url3ds,] = useState<any>("https://pci-api-demo.airwallex.com/pa/card3ds/hk/three-ds-method/redirect/start?key=9fa6a254-be66-4cd9-b465-12ee8dd80c85");
+  const isHidden:boolean= false;
   const key =
     "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6InNkayIsIm1lcmNoYW50UmVmZXJlbmNlIjoiY2FhZjljMDAtMmJlMC00OGExLWJjZWItMGY4YzkyMTdkZjdjIiwiZXhwIjoyMDYwMTE2ODY0LCJpc3MiOiJhcGktdG9rZW4tc2VydmljZSJ9.nONIY-nWnEnOx9DLtSctEK0ZBYAG6U2xFIyWBuZ3B8hXco8M-fcQTjog2vXmi2YDRR_8TgwzlsfO5QB_ouDCNq3yU6hVRAbzBfQT1Jm0tMD0Qvd2Ntx0yQ5JfhOzxaVPGuA-05CJy_Jt-fI58dMZ90WtTQV9VNJI2IXCtL9WVYyai0SSStzvvtJpT4bbN8etIO2aQzAkethmoT_gDwyhnW8qmtMZifl1sS1y8JhwGIWvRPvqtsp-Bupw--lOuCSt2dRMbk193PE0MN9pGaongCfspNf_IEQmkECXJgjAg3j5ft3EUoGJCNmLMDfAYdOcwMBbk5K_bALBwiUIeDMiVNtU2djm774C_Mv8z392BVqn9MPkAcLEGdlNFMlXgLe3DSFu2mjSuUCK3GL102UrdAEzxEbEk6dfTXLbfHhPlebOz8-s4uJyMzEJXG-wW8g18Uly5fha7o74V87eepwtIK8CseMFXuDIIq50WBM6ljaCUxra8dPNueL8Wg0PA6qEJ5MMCxRL8Zj2g8ZcaNAzk8T1VZc-5J8NowtZii5KXv6ZnyqWfoW1QIAHhNcZPyOB_-dU6s7o_7aCtNoD_f0mNjLi6ww-Vvk7vkd5305vmS4d8nciXzNtk6W2DtHhSzVHIxWM3GcwOIHLI4iUbNgwQb0_nA33IUcDLty_K9n1lgU";
   configureSdk(key, "dev");
@@ -41,7 +42,7 @@ const App = () => {
 */
   console.log(flag);
   return (
-    <>
+    <div>
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         <Widget
           type="card"
@@ -93,9 +94,10 @@ const App = () => {
           url={url3ds}
           setRes={setRes}
           onAuthClose={setFlag}
+          isAuth={isHidden}
         />
       )}
-    </>
+    </div>
   );
 };
 
